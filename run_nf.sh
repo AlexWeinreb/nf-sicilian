78b3dc6ee51854fa04be738deff64244ebc0e021
Alexis Weinreb <alexis.weinreb@yale.edu>
2022-10-07 17:11:32 -0400

    increase wrapper memory, load modules

--- run_nf.sh
+++ run_nf.sh
@@ -2,10 +2,15 @@
 #SBATCH --partition=general
 #SBATCH --job-name=nf-sicilian-wrapper
 #SBATCH -c 1
-#SBATCH --mem=50M
+#SBATCH --mem=500M
 #SBATCH --time=5-5:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=alexis.weinreb@yale.edu
+
+module load miniconda
+module load Java/11.0.2
+
+conda activate sicilian_no_nf
 
 nextflow run main.nf -resume -with-dag dag.dot -c 221007_3samples.conf -profile singularity
 
